1263c6e1a651f69a13dec37e3360ec0a7b628385
aswnss-m <aswnss.m@gmail.com>
2020-11-07 15:54:28 +0530

    improved run-length encoding

--- CodingProblems/encoding.py
+++ CodingProblems/encoding.py
@@ -1,21 +1,20 @@
-"""
-Run-length encoding is a fast and simple method of encoding strings. The basic idea is to 
-represent repeated successive charactors as a single count and charactor.
-For example the string,
-"AAAABBBCCDAA" would be
-"4A3B2C1D2A"
-1: start
-2: set the first char as key pos = 0
-3: go throught the string from pos + 1
-4: if char at pos+1 is same as key , count + 1 , pos + 1
-5: else store count and current key , then set key = pos + 1
-6: if pos == len(string) - 1 , stop
-"""
-#usr_input = input()
-#for now
 
-usr_input = "AAAABBBCCDAA"
+# * Run-length encoding
+# Run-length encoding is a fast and simple method of encoding strings. The basic idea is to 
+# represent repeated successive charactors as a single count and charactor.
+# For example the string,
+# "AAAABBBCCDAA" would be
+# "4A3B2C1D2A"
+# 1: start
+# 2: set the first char as key pos = 0
+# 3: go throught the string from pos + 1
+# 4: if char at pos+1 is same as key , count + 1 , pos + 1
+# 5: else store count and current key , then set key = pos + 1
+# 6: if pos == len(string) - 1 , stop
 
+
+
+# Function to do the encoding
 def encode(string):
     output = []
     pos = 0
@@ -28,19 +27,17 @@ def encode(string):
             if char == string[x]:
                 count += 1
                 pos += 1
+            
             else:
-                output.append(str(count))
-                output.append(char)
                 break
+
+        output.append(str(count))
+        output.append(char)
         pos += 1
 
-    output.append(str(count))
-    output.append(char)
-                
     return "".join(output)
 
-encoded = encode(usr_input)
-print(encoded)
 
-#encoded = "4A3B2C1D2A"
-#expected output
+if __name__=='__main__':
+    user_input = input("Enter the string : ")
+    print(encode(user_input))
