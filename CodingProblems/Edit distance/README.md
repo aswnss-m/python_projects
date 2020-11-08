90000c2d6932448f6bbe2c3c0e5c9e2fe38f791f
aswnss-m <aswnss.m@gmail.com>
2020-11-08 11:45:28 +0530

    including edit distance program

--- CodingProblems/Edit distance/README.md
+++ CodingProblems/Edit distance/README.md
@@ -20,4 +20,16 @@ The edit distance between two strings refers to the minimum number of character 
 
 ## Algorithm
 
-<!-- ! Algorithm is still in production>
+1. Start
+
+2. Get both initial and final words
+
+3. Check the lengths of both words and choose the minimum length
+
+4. The difference of the lengths will give the no.of deletions to be done , add it to the distance
+
+5. Keeping the smaller length as a reference check equality of charactors at respective positions of the final word to the initial word , if they are different add the no.of difference to the distance
+
+6. Return the final distance 
+
+7. Stop
--- CodingProblems/Edit distance/edit_distance.py
+++ CodingProblems/Edit distance/edit_distance.py
@@ -1,15 +1,32 @@
 
-def edit_distance(initial, final):
-    
-    pass
+# * Pass the initial word and target word
 
+def edit_distance(initial, target):
+    
+    # ? Finding which word is smaller
+    initial_len = len(initial)
+    target_len = len(target)
+
+    # ? Getting their difference , since thats the no. of letters that we have to edit to get target word
+ 
+    distance = abs(initial_len - target_len)
+
+    if initial_len < target_len : smaller = initial_len 
+    else : smaller = target_len
+
+    # ? Comparing all the letters at respective positions of both words to find whether we should change it or keep it 
+    for pos in range(smaller):
+        if initial[pos] != target[pos]:
+            distance += 1
+        
+    # ! The edit distance is the sum of all the difference we got from above
     return distance
 
 if __name__ == '__main__':
 
     initial = input("Enter the initial word : ")    # The original word
-    final = input("Enter the final word : ")        # The word into which the original must be changed
+    target = input("Enter the target word : ")        # The word into which the original must be changed
 
-    distance = edit_distance(initial,final)
+    distance = edit_distance(initial,target)
 
     print("Edit distance : ", distance)
