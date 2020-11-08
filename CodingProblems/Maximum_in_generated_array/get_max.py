d83966290564dfcb76a34b8517057330afe8bfe7
aswnss-m <aswnss.m@gmail.com>
2020-11-08 12:41:40 +0530

    Made the if condition smaller

--- CodingProblems/Maximum_in_generated_array/get_max.py
+++ CodingProblems/Maximum_in_generated_array/get_max.py
@@ -1,14 +1,14 @@
 def generate_array(size):
     if size == 0:
         return [0]    # * nums = [0] , Maximum will be 0 
-         
+
     nums = [0 for _ in range(size + 1)]
     nums[1] = 1
     
     for i in range(len(nums)):
-        if 2 <= 2 * i and 2 * i <= size:
+        if 2 <= 2 * i <= size:
             nums[2*i] = nums[i]
-        if 2 <= (2 * i) + 1 and (2 * i) + 1 <= size:
+        if 2 <= (2 * i) + 1 <= size:
             nums[(2 * i) + 1] = nums[i] + nums[i+1]
     return nums    
 
